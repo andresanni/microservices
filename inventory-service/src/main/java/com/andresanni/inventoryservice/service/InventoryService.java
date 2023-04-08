84a0e9fa269b07ca9a544a6c12cf8916985ef38b
Andres Anni <andres.anni1985@gmail.com>
2023-04-08 15:03:06 -0300

    Chequeamos que el producto este en existencia antes de guardar la orden. Utilizamos WebClient para consumir el servicio inventory-service desde order-service

--- inventory-service/src/main/java/com/andresanni/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/andresanni/inventoryservice/service/InventoryService.java
@@ -1,12 +1,15 @@
 package com.andresanni.inventoryservice.service;
 
+import com.andresanni.inventoryservice.dto.InventoryResponse;
 import com.andresanni.inventoryservice.model.Inventory;
 import com.andresanni.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,8 +17,13 @@ public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode){
-       Optional<Inventory> optionalInventory = inventoryRepository.findBySkuCode(skuCode);
-       return optionalInventory.isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+       return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+               .map(inventory ->
+                   InventoryResponse.builder()
+                           .skuCode(inventory.getSkuCode())
+                           .isInStock(inventory.getQuantity()>0)
+                           .build()
+               ).collect(Collectors.toList());
     }
 }
--- order-service/src/main/java/com/andresanni/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/andresanni/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.andresanni.orderservice.service;
 
+import com.andresanni.orderservice.dto.InventoryResponse;
 import com.andresanni.orderservice.dto.OrderLineItemdsDto;
 import com.andresanni.orderservice.dto.OrderRequest;
 import com.andresanni.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.andresanni.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -19,6 +22,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
 
     public void placeOrder(OrderRequest orderRequest){
@@ -31,7 +35,27 @@ public class OrderService {
                 .collect(Collectors.toList());
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                                .map(OrderLineItems::getSkuCode)
+                                .collect(Collectors.toList());
+
+        //Call to inventory service, and place order if product is in stock
+            InventoryResponse[] inventoryResponses = webClient.get()
+                        .uri("http://localhost:8082/api/inventory",
+                                uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                        .retrieve()
+                        .bodyToMono(InventoryResponse[].class)
+                        .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(inventoryResponse -> inventoryResponse.isInStock());
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemdsDto orderLineDto) {
